a51f4a99faea003d32551050de1e2619eeb3db1d
auroraphtgrp <sliverknight4869@gmail.com>
2023-06-14 19:56:31 +0700

    First_Commit

--- resources/views/admin/share/css.blade.php
+++ resources/views/admin/share/css.blade.php
@@ -21,3 +21,6 @@
 <link rel="stylesheet" href="/assets_admin/css/dark-theme.css" />
 <link rel="stylesheet" href="/assets_admin/css/semi-dark.css" />
 <link rel="stylesheet" href="/assets_admin/css/header-colors.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+    integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
--- resources/views/admin/share/js.blade.php
+++ resources/views/admin/share/js.blade.php
@@ -2,14 +2,14 @@
 <!-- Bootstrap JS -->
 <script src="/assets_admin/js/bootstrap.bundle.min.js"></script>
 <!--plugins-->
-<script src="/assets_admin/js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+    integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="/assets_admin/plugins/simplebar/js/simplebar.min.js"></script>
 <script src="/assets_admin/plugins/metismenu/js/metisMenu.min.js"></script>
 <script src="/assets_admin/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-<script src="/assets_admin/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-<script src="/assets_admin/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script src="/assets_admin/plugins/chartjs/js/Chart.min.js"></script>
-<script src="/assets_admin/plugins/chartjs/js/Chart.extension.js"></script>
-<script src="/assets_admin/js/index.js"></script>
 <!--app JS-->
 <script src="/assets_admin/js/app.js"></script>
